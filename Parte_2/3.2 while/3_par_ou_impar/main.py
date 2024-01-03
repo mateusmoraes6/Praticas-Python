b5419705288a59d66fa71212f376ed8cf8f3a1dc
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-01-03 16:20:30 -0300

    add práticas 3.2 while

--- Parte_2/3.2 while/3_par_ou_impar/main.py
+++ Parte_2/3.2 while/3_par_ou_impar/main.py
@@ -1,23 +1,37 @@
 from random import randint
 computador = randint(0, 10)
+cont = 0
 
-valor = int(input('Diga um valor: '))
-parOuImpar = str(input('Par ou Ímpar? [P/I] ')).upper()
-
-soma = computador + valor
-
-if parOuImpar == 'P':
-    if soma %2 == 0:
-        print(f'Você jogou {valor} e o computador {computador}. Total de {soma} que é PAR!')
-    elif soma %2 == 1:
-        print(f'Você jogou {valor} e o computador {computador}. Total de {soma} que é ÌMPAR!')
-    print('Você VENCEU!')
-    print('Vamos jogar novamente...')
-elif parOuImpar == 'I':
-    if soma %2 == 0:
-        print(f'Você jogou {valor} e o computador {computador}. E o total é {soma}, que é PAR!')
-    elif soma %2 == 1:
-        print(f'Você jogou {valor} e o computador {computador}. E o total é {soma}, que é ÌMPAR!')
-    print('Você PERDEU!')
-    print('GAMER OVER!')
-
+print('VAMOS JOGAR ÍMPAR OU PAR!')
+while True:
+    print('*' * 25)
+    valor = int(input('Diga um valor: '))
+    parOuImpar = str(input('Par ou Ímpar? [P/I] ')).upper()
+    print('-' * 25)
+    soma = computador + valor
+    if parOuImpar == 'P':
+        if soma %2 == 0:
+            print(f'Você jogou {valor} e o computador {computador}. Total de {soma} que é PAR!')
+            print('-' * 25)
+            print('Você VENCEU!')
+            print('Vamos jogar novamente...')
+            cont += 1
+        elif soma %2 == 1:
+            print(f'Você jogou {valor} e o computador {computador}. Total de {soma} que é ÌMPAR!')
+            print('Você PERDEU!')
+            print('-' * 25)
+            print(f'GAMER OVER! Você venceu {cont} vezes.')
+            break
+    elif parOuImpar == 'I':
+        if soma %2 == 0:
+            print(f'Você jogou {valor} e o computador {computador}. E o total é {soma}, que é PAR!')
+            print('Você PERDEU!')
+            print('-' * 25)
+            print(f'GAMER OVER! Você venceu {cont} vezes.')
+            break
+        elif soma %2 == 1:
+            print(f'Você jogou {valor} e o computador {computador}. E o total é {soma}, que é ÌMPAR!')
+            print('-' * 25)
+            print('Você VENCEU!')
+            print('Vamos jogar novamente...')
+            cont += 1
---
+++ Parte_2/3.2 while/4_análise_de_dados_do_grupo/main.py
@@ -0,0 +1,29 @@
+contIdade = contMasc = contFem = 0
+continua = 'S'
+
+while continua == 'S':
+    print('-' * 30)
+    print('     CADASTRO DE PESSOAS')
+    print('-' * 30)
+    idade = int(input('Idade: '))
+    while idade <= 0:
+        idade = int(input('Idade: '))
+    if idade >= 18:
+        contIdade += 1
+    sexo = str(input('Sexo: [M/F] ')).upper()
+    while sexo != 'M' and sexo != 'F':
+        sexo = str(input('Sexo: [M/F] ')).upper()
+    if sexo == 'M':
+        contMasc += 1
+    elif sexo == 'F' and idade < 20:
+        contFem += 1
+    print('-' * 30)
+    continua = str(input('Continuar cadastro? [S/N] ')).upper()
+    while continua != 'S' and continua != 'N':
+        continua = str(input('Continuar cadastro? [S/N] ')).upper()
+print('-' * 30)
+print('     CADASTRO ENCERRADO')
+print('-' * 30)
+print(f'''Total de pessoas com 18 anos ou mais: {contIdade}.
+Total de homens cadastrados: {contMasc}.
+Total de mulheres com menos de 20 anos: {contFem}.''')
